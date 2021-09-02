6f134ffb3e09245d7d8c391a965c471697927e2c
arnab0000007 <arnab0000007@gmail.com>
2021-09-02 22:54:24 +0600

    some function added

--- js/api.js
+++ js/api.js
@@ -1,10 +1,6 @@
-// toggle spinner function
-const toggleSpinner = (displayStyle) => {
-  document.getElementById("spinner").style.display = displayStyle;
-};
-// toggle search result
-const toggleSearchResult = (displayStyle) => {
-  document.getElementById("books").style.display = displayStyle;
+// toggle function for spinner,error,disply
+const toggle = (divId, displayStyle) => {
+  document.getElementById(divId).style.display = displayStyle;
 };
 // search book function
 const searchBook = () => {
@@ -13,15 +9,17 @@ const searchBook = () => {
   // clear data of input field
   searchField.value = "";
   if (searchText == "") {
-    alert("Please write something");
+    toggle("error-message", "block");
   } else {
+    toggle("spinner", "block");
+    toggle("books", "none");
+    toggle("error-message", "none");
     // load data
-    toggleSpinner("block");
-    toggleSearchResult("none");
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => displaySearchResult(data));
+      .then((data) => displaySearchResult(data))
+      .catch((error) => console.log(error));
   }
 };
 // display result function
@@ -41,13 +39,14 @@ const displaySearchResult = (books) => {
     <h3>No Results Found</h3>
     `;
     totalResult.appendChild(totalDiv);
-    toggleSpinner("none");
-    toggleSearchResult("block");
+    toggle("spinner", "none");
+    toggle("books", "block");
   } else {
     totalDiv.innerHTML = `
     <h3>Total ${books.numFound} results found</h3>
     `;
-    books.docs?.forEach((book) => {
+    const displayBooks = books.docs.slice(0, 30);
+    displayBooks?.forEach((book) => {
       const div = document.createElement("div");
       div.classList.add("col");
       div.innerHTML = `
@@ -64,7 +63,7 @@ const displaySearchResult = (books) => {
         book.subtitle ? ": " + book.subtitle : ""
       }</h5>
                   <p class="card-text">Author: ${
-                    book.author_name ? book.author_name[0] : "Unknown Author"
+                    book.author_name ? book.author_name[0] : "Unknown"
                   }</p>
                   <p class="card-text">First published in: ${
                     book.first_publish_year
@@ -74,7 +73,7 @@ const displaySearchResult = (books) => {
       
                   <small>
                   Publisher: ${
-                    book.publisher ? book.publisher[0] : "Not Available"
+                    book.publisher ? book.publisher[0] : "Not available"
                   }
                   </small>
           </div>
@@ -84,7 +83,7 @@ const displaySearchResult = (books) => {
       `;
       searchResult.appendChild(div);
     });
-    toggleSpinner("none");
-    toggleSearchResult("block");
+    toggle("spinner", "none");
+    toggle("books", "block");
   }
 };
